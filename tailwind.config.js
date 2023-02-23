0b793ffa822f3642fcb55da0fa4ecfdce34b0886
juansechalarca <juansechalarca16@gmail.com>
2023-02-23 14:34:54 -0500

    feat: create CSS architecture

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
   theme: {
+    fontFamily: {
+      display: ['Oswald', 'sans-serif'],
+      body: ['Poppins', 'sans-serif'],
+    },
+    container: {
+      center: true,
+      padding: '1.5rem',
+    },
     extend: {},
   },
   plugins: [],
-}
+};
